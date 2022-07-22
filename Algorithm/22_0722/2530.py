0dc4fc98b621c77132f44e8f768af0b238120871
‍원정수[학생](전자정보대학 전자공학과) <waterpurifier@khu.ac.kr>
2022-07-22 14:04:25 +0000

    22.07.22

---
+++ Algorithm/22_0722/2530.py
@@ -0,0 +1,17 @@
+### 인공지능 시계 #2530
+
+A, B, C = map(int, input().split())
+
+D = int(input())
+
+C += D
+
+if C >= 60:
+    B += C // 60
+    C = C % 60
+    if B >= 60:
+        A += B // 60
+        B = B % 60
+        if A >= 24:
+            A = A % 24
+print(A, B, C)
